4575aac8e71ef3235a2aaab4ebe09877c8f60773
Sukkithagunathilake <sukithagunathilake@gmail.com>
2025-03-01 17:30:15 +0530

    mongo db server connection

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+// Load environment variables
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
+
+const app = express();
+app.use(express.json());
+
+//routes
+app.get("/", (req, res) => res.send("API is running..."));
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

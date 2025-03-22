62f9fa2d2073ce4737d6f84a5a8a49853ac1a6d0
Sukkithagunathilake <sukithagunathilake@gmail.com>
2025-03-22 12:54:33 +0530

    mar2212.54

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -6,7 +6,6 @@ import image2 from "../assets/image2.png";
 import image3 from "../assets/image3.png";
 import image4 from "../assets/image4.png";
 import image5 from "../assets/image5.png";
-import image6 from "../assets/image6.png";
  
 
 

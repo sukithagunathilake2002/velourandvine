d2e4c84b7c5f16e60f72ebf69261afd80150c6a6
chanuth jayasekera <chanuthjayasekera33@gmail.com>
2025-05-03 22:45:48 -0700

    changes

--- frontend/src/components/CStaffOrderPage.js
+++ frontend/src/components/CStaffOrderPage.js
@@ -5,7 +5,7 @@ import io from "socket.io-client";
 import jsPDF from "jspdf";
 import autoTable from "jspdf-autotable"; // ✅ Correct import
 import '../styles/CStaffOrderPage.css';
-import io from "socket.io-client"; // Import Socket.IO client
+ // Import Socket.IO client
 
 
 
